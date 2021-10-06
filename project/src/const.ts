f013355a216cdc4d9ad96a32c6d71a6d1d51c051
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2021-10-06 10:20:49 +0300

    Refactoring named exports

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,11 +1,13 @@
-export const AppRoute = {
+const AppRoute = {
   MainScreen: '/',
   LoginScreen: '/login',
   FavoritesScreen: '/favorites',
   OfferScreen: '/offer/:id',
 } as const;
 
-export const AuthorizationStatus = {
+const AuthorizationStatus = {
   Auth: 'AUTH',
   NoAuth: 'NO_AUTH',
 } as const;
+
+export {AppRoute, AuthorizationStatus};
