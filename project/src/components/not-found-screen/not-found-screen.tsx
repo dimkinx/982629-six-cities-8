b8d564c18ae37b6e99cae1a6dd794397b5b0245b
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2021-10-15 09:40:18 +0300

    Refactoring NotFoundScreen component

--- project/src/components/not-found-screen/not-found-screen.tsx
+++ project/src/components/not-found-screen/not-found-screen.tsx
@@ -1,18 +1,18 @@
-import {useHistory, useLocation} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import {AppRoute} from '../../common/const';
-import './style.scss';
+import './style.css';
 
 function NotFoundScreen(): JSX.Element {
   const location = useLocation();
-  const history = useHistory();
 
   return (
     <div className="wrapper-404">
       <h1 className="title-404" data-text="404">404</h1>
-      <p className="description-404">The page at <b>{location.pathname}</b> - not found.</p>
-      <p className="sub-description-404">
-        You may {(history.length > 1) && <><button onClick={() => history.goBack()} type="button">go back</button> or </>}
-        return to <button onClick={() => history.push(AppRoute.MainScreen)} type="button">main page</button>
+      <p className="description-404">
+        The page at <b>{location.pathname}</b> - not found!
+      </p>
+      <p className="description-404">
+        Return to <Link to={AppRoute.MainScreen} className="description-404__link">main page</Link>.
       </p>
     </div>
   );
