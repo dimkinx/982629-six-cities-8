3f229391a42cc0926c3cde45fd7d0a4d1505bbba
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2021-11-01 17:00:24 +0300

    Connect redux

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from '@reduxjs/toolkit';
+import {reducer} from './store/reducer';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offers = {offers}
-      reviews = {reviews}
-    />
+    <Provider store={store}>
+      <App
+        offers = {offers}
+        reviews = {reviews}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
