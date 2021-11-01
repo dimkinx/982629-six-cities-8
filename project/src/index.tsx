2ee477f582ccb0bce06dd91e1d77ff737907e8c5
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2021-11-01 17:03:52 +0300

    Add support for Redux DevTools

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import {createStore} from '@reduxjs/toolkit';
 import {reducer} from './store/reducer';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
